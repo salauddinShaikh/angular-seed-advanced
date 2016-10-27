92b180235f741033a0915695a409920903f66b8f
SagarShelar <sagar.shelar@eternussolutions.com>
2016-10-27 12:24:55 +0530

    Update components.

--- src/client/app/components/app.routes.ts
+++ src/client/app/components/app.routes.ts
@@ -1,8 +1,16 @@
 // app
 import { HomeRoutes } from './home/home.routes';
 import { AboutRoutes } from './about/about.routes';
+import { DashboardRoutes } from './dashboard/dashboard.routes';
+import { AppRoutes } from './app/app.routes';
+import { LmsRoutes } from './lms/lms.routes';
+import { TimesheetRoutes } from './timesheet/timesheet.routes';
 
 export const routes: Array<any> = [
   ...HomeRoutes,
-  ...AboutRoutes
+  ...AppRoutes,
+  ...AboutRoutes,
+  ...DashboardRoutes,
+  ...LmsRoutes,
+  ...TimesheetRoutes
 ];
