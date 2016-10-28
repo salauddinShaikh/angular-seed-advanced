f2f33fa819e2f54e550dcbe3975ac554e4094691
MT <tilak.mukul@hotmail.com>
2016-10-28 13:01:44 +0530

    ApplyLeave Form

--- src/client/app/components/lms/lms-applyleave/lms-applyleave.component.ts
+++ src/client/app/components/lms/lms-applyleave/lms-applyleave.component.ts
@@ -1,4 +1,5 @@
 import { BaseComponent } from '../../../frameworks/core/index';
+import { SelectItem } from 'primeng/primeng';
 
 
 @BaseComponent({
@@ -8,5 +9,19 @@ import { BaseComponent } from '../../../frameworks/core/index';
 })
 export class LmsApplyLeavesComponent {
 
+  leaves : SelectItem[];
+  selectedLeave : string;
+  numberofdays :string;
+  start : Date;
+  end : Date;
+
+  constructor(){
+    this.leaves = [];
+    this.leaves.push({label:'Select', value:null});
+    this.leaves.push({label:'Leave', value:{id:1, name: 'Leave'}});
+    this.leaves.push({label:'Half Day Leave', value:{id:2, name: 'Half Day Leave'}});
+    this.leaves.push({label:'Absent', value:{id:3, name: 'Absent'}});
+    this.leaves.push({label:'Half Day Absent', value:{id:4, name: 'Half Day Absent'}});
+  }
 
 }
