acc676ec8dd8ef6336afe1d1e206c3808763fbf1
SagarShelar <sagar.shelar@eternussolutions.com>
2016-11-02 12:56:03 +0530

    Added reference of namelist back

--- src/client/web.module.ts
+++ src/client/web.module.ts
@@ -112,7 +112,7 @@ export function cons() {
     }),
     EffectsModule.run(MultilingualEffects),
     EffectsModule.run(TimeSheetEffects),    
-    //EffectsModule.run(NameListEffects),
+    EffectsModule.run(NameListEffects),
     ScheduleModule,
     DataTableModule,
     SharedModule,
