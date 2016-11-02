ece973330b647702faed66a5a48b1e9537f9e11d
salauddin <sagar.shelar@eternussolutions.com>
2016-11-02 13:03:42 +0530

    implemented getUserPermission API

--- src/client/app/components/login/login.component.ts
+++ src/client/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { BaseComponent } from '../../frameworks/core/index';
 import { LoginService } from '../../shared/services/login.service';
 import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+
 
 @BaseComponent({
     moduleId: module.id,
@@ -8,21 +10,26 @@ import { Router } from '@angular/router';
     templateUrl: 'login.component.html',
     styleUrls: ['login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
     public errorMessage: string;
     private model: User;
     constructor(private _loginService: LoginService, private _router: Router) {
-      this.model = new User('', '');
+        this.model = new User('', '');
+    }
+    ngOnInit() {
+        if (localStorage.getItem('accessToken') !== null) {
+            this._router.navigate(['/']);
+        }
     }
     doLogin(): void {
         this._loginService.authenticate(this.model)
             .subscribe(
             results => {
-                //this.getLoggedInUserPermission();
-                this._router.navigate(['/']);
+                this.getLoggedInUserPermission();
+                //this._router.navigate(['/']);
             },
             error => {
-            this.errorMessage = <any>error;
+                this.errorMessage = <any>error;
             });
     }
     getLoggedInUserPermission(): void {
@@ -34,9 +41,9 @@ export class LoginComponent {
             error => this.errorMessage = <any>error);
     }
 }
-class User{
-  constructor(
-    public Password: string,
-    public UserName: string
-  ) {  }
+class User {
+    constructor(
+        public Password: string,
+        public UserName: string
+    ) { }
 }
