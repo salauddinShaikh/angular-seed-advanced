8179b0d1c6550fdcaeec12d0872ad2b524c6ff4a
MT <tilak.mukul@hotmail.com>
2016-11-03 17:46:36 +0530

    lms-applyleave.component.html ifAuthorize implement

--- src/server/userData.js
+++ src/server/userData.js
@@ -6,9 +6,12 @@ var users = [
         Password: 'password',
         Permissions: [
             'Timesheet.READ','Timesheet.CREATE','Timesheet.UPDATE','Timesheet.DELETE',
+            
+            'ConferenceBook.READ','ConferenceBook.CREATE','ConferenceBook.UPDATE','ConferenceBook.DELETE',
             'Leave.READ','Leave.CREATE','Leave.UPDATE','Leave.DELETE',
             'Holiday.READ','Holiday.CREATE','Holiday.UPDATE','Holiday.DELETE',
-            'ConferenceBook.READ','ConferenceBook.CREATE','ConferenceBook.UPDATE','ConferenceBook.DELETE',
+            'ApprovalLeave.READ','ApprovalLeave.CREATE','ApprovalLeave.UPDATE','ApprovalLeave.DELETE',
+            'BulkApprovalLeave.READ','BulkApprovalLeave.CREATE','BulkApprovalLeave.UPDATE','BulkApprovalLeave.DELETE',
              ]
     },
     {
@@ -28,5 +31,30 @@ var users = [
         UserName: 'user3',
         Password: 'password',
         Permissions: [   'ConferenceBook.READ','ConferenceBook.CREATE','ConferenceBook.UPDATE','ConferenceBook.DELETE' ]
+    },
+
+    {
+        Id: 14,
+        UserName: 'user4',
+        Password: 'password',
+        Permissions: ['Leave.READ','Holiday.READ','Leave.CREATE','ApprovalLeave.READ','BulkApprovalLeave.READ']
+    },
+    {
+        Id: 15,
+        UserName: 'user5',
+        Password: 'password',
+        Permissions: ['Leave.READ','Leave.CREATE',]
+    },
+    {
+        Id: 16,
+        UserName: 'user6',
+        Password: 'password',
+        Permissions: ['Leave.READ','ApprovalLeave.READ','BulkApprovalLeave.READ']
+    },
+    {
+        Id: 17,
+        UserName: 'user7',
+        Password: 'password',
+        Permissions: ['Leave.READ','Holiday.READ','ApprovalLeave.READ']
     }];
 module.exports = users;
