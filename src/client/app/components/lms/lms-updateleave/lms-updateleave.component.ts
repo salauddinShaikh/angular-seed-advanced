1bf24b1c9ccf0f5b0c7875131000696a72b6d1a6
MT <tilak.mukul@hotmail.com>
2016-11-03 10:59:15 +0530

    LmsUpdateLeavesComponent syntax correction

--- src/client/app/components/lms/lms-updateleave/lms-updateleave.component.ts
+++ src/client/app/components/lms/lms-updateleave/lms-updateleave.component.ts
@@ -11,15 +11,15 @@ export class LmsUpdateLeavesComponent {
   leaves : any[];
   managers :any[];
 
-  ngOnInit(){
+  ngOnInit() {
     this.leaves = [
-      { type:"Leave", numberofleaves:1, reason:"Personal", start:"22-09-2016", end:"22-09-2016", action:""},
-      { type:"Leave", numberofleaves:1, reason:"Personal", start:"23-09-2016", end:"23-09-2016", action:""},
+      { type:'Leave', numberofleaves:1, reason:'Personal', start:'22-09-2016', end:'22-09-2016', action:''},
+      { type:'Leave', numberofleaves:1, reason:'Personal', start:'23-09-2016', end:'23-09-2016', action:''},
       ];
 
       this.managers =[
-        {project:"HR", manager:"Pooja Merchant", status:"Approved", comments:"Approved"},
-        {project:"RMS", manager:"Sagar Shelar", status:"Approved", comments:"Approved"},
+        {project:'HR', manager:'Pooja Merchant', status:'Approved', comments:'Approved'},
+        {project:'RMS', manager:'Sagar Shelar', status:'Approved', comments:'Approved'},
       ];
   }
 
