ba51b84c84f4dbe09e0faca85be54756ec72f34f
MT <tilak.mukul@hotmail.com>
2016-11-03 10:58:22 +0530

    LmsLeavesComponent syntax correction

--- src/client/app/components/lms/lms-myleaves/lms-leaves.component.ts
+++ src/client/app/components/lms/lms-myleaves/lms-leaves.component.ts
@@ -11,11 +11,11 @@ export class LmsLeavesComponent {
     servRows = 5;
     leaves :any[];
 
-    ngOnInit(){
+    ngOnInit() {
       this.leaves = [
-        {employee:"Person1 LName", startdate:"22-09-2016", enddate:"23-09-2016", numberofleaves:2, status:"Approved"},
-        {employee:"Person1 LName", startdate:"22-08-2016", enddate:"22-08-2016", numberofleaves:1, status:"Approved"},
-        {employee:"Person1 LName", startdate:"02-10-2016", enddate:"03-10-2016", numberofleaves:2, status:"Approved"}
+        {employee:'Person1 LName', startdate:'22-09-2016', enddate:'23-09-2016', numberofleaves:2, status:'Approved'},
+        {employee:'Person1 LName', startdate:'22-08-2016', enddate:'22-08-2016', numberofleaves:1, status:'Approved'},
+        {employee:'Person1 LName', startdate:'02-10-2016', enddate:'03-10-2016', numberofleaves:2, status:'Approved'}
       ];
     }
 }
