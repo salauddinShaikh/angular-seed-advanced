6bf9a16b6600614677cc68757ed23ae9b470fce5
Amol Patil <Amol.Patil>
2016-11-10 13:05:23 +0530

    Added Profile Details html

---
+++ src/client/app/components/profile/components/profile-details/profile-details.component.ts
@@ -0,0 +1,25 @@
+/** Angular Dependencies */
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+/** Framework Dependencies */
+import {BaseComponent} from '../views/base-component';
+
+/** Module Level Dependencies */
+
+
+/** Component Declaration */
+@BaseComponent({
+  moduleId: module.id,
+  selector: 'profile-details',
+  templateUrl: 'profile-details.component.html'
+})
+export class ProfileDetailsComponent implements OnInit {
+
+  constructor(
+    private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+}
