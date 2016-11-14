73dd826ce666fffb1728b66970152ff8f5b09405
MT <tilak.mukul@hotmail.com>
2016-11-14 17:03:03 +0530

    lms-applyleave test cases changes

--- src/client/app/components/lms/lms-applyleave/lms-applyleave.component.spec.ts
+++ src/client/app/components/lms/lms-applyleave/lms-applyleave.component.spec.ts
@@ -5,47 +5,75 @@ import { TestBed } from '@angular/core/testing';
 // app
 import { t } from '../../../frameworks/test/index';
 import { LmsApplyLeavesComponent } from './lms-applyleave.component';
- console.log('test check');
+console.log('test check');
 // test module configuration for each test
 const testModuleConfig = () => {
-  TestBed.configureTestingModule({
-    declarations: [LmsApplyLeavesComponent, TestComponent]
-  });
+    TestBed.configureTestingModule({
+        declarations: [LmsApplyLeavesComponent, TestComponent]
+    });
 };
 
- export function main(){
-     console.log('test check');
-    t.describe('@Component:LmsApplyLeavesComponent',()=>{
+export function main() {
+    console.log('test check');
+    t.describe('@Component:LmsApplyLeavesComponent', () => {
         t.be(testModuleConfig);
 
-        t.it('should work',t.async(()=>{
-            TestBed.compileComponents().then(()=>{
-                
+        t.it('should work', t.async(() => {
+            TestBed.compileComponents().then(() => {
+
                 let fixture = TestBed.createComponent(TestComponent);
                 fixture.detectChanges();
                 let compiled = fixture.debugElement.nativeElement;
 
-                t.it('should find page content',()=>{
+                let componentInstance = fixture.debugElement.children[0].componentInstance;
+
+                t.it('should find page content', () => {
                     t.e(compiled.querySelectorAll()[0]).toBeTruthy();
                 });
 
-                //let instance = fixture.debugElement.children[0].componentInstance;
-                t.it('should do math',()=>{
-                    t.e(1+1).toBe(2);
-                    console.log('control did the math');
+                t.it('checks page-load component status',()=>{
+                    var date = new Date();
+                    t.e(compiled.querySelector('span')[1]).toBe('Apply Leave');
+                    t.e(compiled.querySelector('h5')).toBe('');
+                    t.e(componentInstance.warning).toBe('');
+                    t.e(componentInstance.finalLeaveData.length).toBe(0);
+                    t.e(compiled.querySelectorAll('input')[1].getAttribute('value')).toBe('');
+                    t.e(componentInstance.showNumDays).toBeFalsy();
+                    t.e(compiled.querySelectorAll('input')[2].getAttribute('value')).toBe(date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear());
+                    t.e(componentInstance.start).toBeFalsy(date);
+                    t.e(compiled.querySelectorAll('input')[3].getAttribute('value')).toBe(date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear());
+                    t.e(componentInstance.end).toBeFalsy(date);
+                    t.e(compiled.querySelector('textarea').getAttribute('value')).toBe('');
+                    t.e(componentInstance.comments).toBe('');
                 });
+
+                t.it('check blank values validation for Add Leave',()=>{
+                    t.e(compiled.querySelector('button')['Add Leave']).click();
+                    t.e(compiled.querySelector('h5')).toBeTruthy();
+
+                    componentInstance.cancelPressed();
+
+                    t.e(compiled.querySelector('h5')).toBeFalsy();
+
+                });
+
+                t.it('check blank values validation for Cancel',()=>{
+                    t.e(compiled.querySelector('button')['Cancel']).click();
+                    t.e(compiled.querySelector('h5')).toBeFalsy();
+                });
+
             });
         }));
     });
-  }
+}
 
 
 
 @Component({
-  selector: 'test-cmp',
-  template: '<sd-applyleave></sd-applyleave>'
+    selector: 'test-cmp',
+    template: '<sd-applyleave></sd-applyleave>'
 })
-class TestComponent{}
+class TestComponent { }
 
 
 //                 let compiled = fixture.debugElement.nativeElement;
